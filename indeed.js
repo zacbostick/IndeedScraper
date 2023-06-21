faea0aaa89d8660c251a6955ceb623a74c2137c7
Zac <zac.bostick@gmail.com>
2023-06-21 17:37:34 -0500

    Improving Speed with Parallelization+

--- indeed.js
+++ indeed.js
@@ -9,6 +9,7 @@ dotenv.config();
 puppeteerExtra.use(StealthPlugin());
 
 const maxJobCards = 150;
+
 const autoScroll = async (page) => {
   await page.evaluate(async () => {
     await new Promise((resolve) => {
@@ -23,9 +24,47 @@ const autoScroll = async (page) => {
           clearInterval(timer);
           resolve();
         }
-      }, 300);
+      }, 100);
     });
   });
+};
+
+const scrapePage = async (browser, url, userAgent) => {
+  const page = await browser.newPage();
+  await page.setRequestInterception(true);
+  page.on('request', (req) => {
+    if(req.resourceType() === 'font' || req.resourceType() === 'image' || req.resourceType() === 'stylesheet'){
+      req.abort();
+    } else {
+      req.continue();
+    }
+  });
+  await page.setUserAgent(userAgent);
+  await page.setCookie({ name: 'cookieName', value: 'cookieValue', url: 'https://www.indeed.com' });
+  page.setDefaultNavigationTimeout(120000);
+  await page.goto(url, { waitUntil: 'domcontentloaded' });
+  await autoScroll(page);
+  const jobCardHTMLs = await page.evaluate(() =>
+    Array.from(document.querySelectorAll('.job_seen_beacon')).map((el) => el.outerHTML)
+  );
+
+  const jobDetails = jobCardHTMLs.map((html) => {
+    const $ = cheerio.load(html);
+    return {
+      title: $('h2.jobTitle span').attr('title'),
+      company: $('.companyName').text(),
+      location: $('.companyLocation').text(),
+      estimatedSalary: $('.estimated-salary span').last().text(),
+      jobType: $('.attribute_snippet').text(),
+      description: $('.job-snippet ul li').map((i, el) => $(el).text()).get().join(' '),
+      datePosted: $('.date').text(),
+      link: 'https://www.indeed.com' + $('a.jcs-JobTitle').attr('href'),
+    };
+  });
+
+  await page.close();
+
+  return jobDetails;
 };
 
 const run = async (searchTerm) => {
@@ -36,7 +75,6 @@ const run = async (searchTerm) => {
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15',
     ];
-    
 
     browser = await puppeteer.launch({
       headless: true,
@@ -44,17 +82,7 @@ const run = async (searchTerm) => {
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
-    const page = await browser.newPage();
     const randomUserAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
-    await page.setUserAgent(randomUserAgent);
-
-    await page.setCookie({
-      name: 'cookieName',
-      value: 'cookieValue',
-      url: 'https://www.indeed.com',
-    });
-
-    page.setDefaultNavigationTimeout(120000);
 
     let jobDetails = [];
     let start = 0;
@@ -64,53 +92,21 @@ const run = async (searchTerm) => {
     console.log('Scraping started...');
 
     while (hasNextPage && totalJobCards < maxJobCards) {
-      let url = `https://www.indeed.com/jobs?q=${encodeURIComponent(searchTerm)}&start=${start}`;
-
-      console.log('Page URL:', url);
-
-      await page.goto(url, { waitUntil: 'domcontentloaded' });
-      console.log('Page loaded');
-
-      await page.waitForTimeout(1000);
-
-      await autoScroll(page);
-      console.log('Page scrolled');
-
-      await page.waitForTimeout(1000);
-
-      const jobCardHTMLs = await page.evaluate(() =>
-        Array.from(document.querySelectorAll('.job_seen_beacon')).map((el) => el.outerHTML)
-      );
-
-      console.log('Job card HTMLs:', jobCardHTMLs.length);
-
-      if (totalJobCards + jobCardHTMLs.length > maxJobCards) {
-        jobCardHTMLs.splice(maxJobCards - totalJobCards);
+      let tasks = [];
+      for(let i = 0; i < 7; i++){
+        let url = `https://www.indeed.com/jobs?q=${encodeURIComponent(searchTerm)}&start=${start + i*15}`;
+        tasks.push(scrapePage(browser, url, randomUserAgent));
       }
 
-      jobDetails.push(
-        ...jobCardHTMLs.map((html) => {
-          const $ = cheerio.load(html);
-          return {
-            title: $('h2.jobTitle span').attr('title'),
-            company: $('.companyName').text(),
-            location: $('.companyLocation').text(),
-            estimatedSalary: $('.estimated-salary span').last().text(),
-            jobType: $('.attribute_snippet').text(),
-            description: $('.job-snippet ul li').map((i, el) => $(el).text()).get().join(' '),
-            datePosted: $('.date').text(),
-            link: 'https://www.indeed.com' + $('a.jcs-JobTitle').attr('href'),
-          };
-        })
-      );
+      let results = await Promise.all(tasks);
+      jobDetails.push(...results.flat());
 
-      totalJobCards += jobCardHTMLs.length;
-      console.log('Total job cards:', totalJobCards);
-
-      hasNextPage = await page.evaluate(() => !!document.querySelector('[aria-label="Next Page"]'));
-      start += 10;
+      totalJobCards += results.reduce((sum, res) => sum + res.length, 0);
+      start += 15 * tasks.length;
 
       console.log(`Scraped ${totalJobCards} job cards...`);
+
+      hasNextPage = results.some(res => !!res.length); // if any page has results, there is a next page
     }
 
     await fs.promises.writeFile('job_details.json', JSON.stringify(jobDetails, null, 2));
@@ -138,5 +134,4 @@ const run = async (searchTerm) => {
 };
 
 const searchTerm = process.argv[2] || 'Computer';
-
 run(searchTerm);
